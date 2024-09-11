2fe0e76ae766895ca5effa0f8b4f4ab426cb82ac
Smith Omovie <omoviesmith@gmail.com>
2024-09-11 19:42:58 -0400

    updated style

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -548,7 +548,7 @@ function ProductList() {
         textDecoration: 'none',
         cursor: 'pointer',
     };
-    
+
     // const styleA = {
     //     color: 'white',
     //     fontSize: '30px',
@@ -596,8 +596,26 @@ function ProductList() {
                 </div>
                 <div style={styleObjUl}>
                     <div><a href="#" onClick={(e) => handlePlantsClick(e)} style={styleA}>Plants</a></div>
-                    <div style={{ position: 'relative' }}>
+                    {/* <div style={{ position: 'relative' }}>
                         <a href="#" onClick={(e) => handleCartClick(e)} style={styleA}>
+                            <h1 className="cart">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
+                                    <rect width="256" height="256" fill="none"></rect>
+                                    <circle cx="80" cy="216" r="12"></circle>
+                                    <circle cx="184" cy="216" r="12"></circle>
+                                    <path d="M42.3,72H221.7l-26.4,92.4A15.9,15.9,0,0,1,179.9,176H84.1a15.9,15.9,0,0,1-15.4-11.6L32.5,37.8A8,8,0,0,0,24.8,32H8" fill="none" stroke="#faf9f9" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" id="mainIconPathAttribute"></path>
+                                </svg>
+                                {totalItemsInCart > 0 && (
+                                    <span className="cart-count">{totalItemsInCart}</span>
+                                )}
+                            </h1>
+                        </a>
+                    </div> */}
+                        <div style={{ position: 'relative' }}>
+                        <a
+                            href="#"
+                            onClick={(e) => handleCartClick(e)}
+                            style={styleA}>
                             <h1 className="cart">
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256" id="IconChangeColor" height="68" width="68">
                                     <rect width="256" height="256" fill="none"></rect>
