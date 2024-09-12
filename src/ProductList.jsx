a0f4fa1f2a25c8a4e2277a27d40d79a3a43df8a0
Smith Omovie <omoviesmith@gmail.com>
2024-09-11 21:07:46 -0400

    handling cart

--- src/ProductList.jsx
+++ src/ProductList.jsx
@@ -643,9 +643,17 @@ function ProductList() {
                                         <div className="product-title">{plant.name}</div>
                                         <div className="product-description">{plant.description}</div>
                                         <div className="product-cost">{plant.cost}</div>
-                                        <button className="product-button" onClick={() => handleAddToCart(plant)}>
+                                        {/* <button className="product-button" onClick={() => handleAddToCart(plant)}>
+                                            {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
+                                        </button> */}
+
+                                        <button className={`product-button ${addedToCart[plant.name] ? 'added-to-cart' : ''}`}
+                                            onClick={() => handleAddToCart(plant)}
+                                            disabled={!!addedToCart[plant.name]}>
                                             {addedToCart[plant.name] ? 'Added to Cart' : 'Add to Cart'}
                                         </button>
+
+
                                     </div>
                                 ))}
                             </div>
